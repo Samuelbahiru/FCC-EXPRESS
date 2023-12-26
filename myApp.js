52f99baabb7732ff10e2bf36e7024aaa2674e925
samuelbahiru <samuelbahiru93@gmail.com>
2023-12-26 10:55:52 +0300

    Part 3

--- myApp.js
+++ myApp.js
@@ -4,8 +4,9 @@ let app = express();
 console.log("Hello World");
 
 app.get("/", (req, res) => {
-  console.log("Hello Express");
-  res.send("Hello Express");
+  //   console.log("Hello Express");
+  //   res.send("Hello Express");
+  res.sendFile(__dirname, "/views/index.html");
 });
 
 module.exports = app;
