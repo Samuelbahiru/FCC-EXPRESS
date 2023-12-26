b78e1efe65f2457a12b4ce00f8bab9cbf7cb3746
samuelbahiru <samuelbahiru93@gmail.com>
2023-12-26 11:05:14 +0300

    Part 3 with little modification

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ console.log("Hello World");
 app.get("/", (req, res) => {
   //   console.log("Hello Express");
   //   res.send("Hello Express");
-  res.sendFile(__dirname, "/views/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 module.exports = app;
